33ff029808e8cea736ea622d8097c50871b58f51
wangwenbo <wangwenbo000@me.com>
2015-12-22 17:58:42 +0800

    add swiper

--- components/index/index.vue
+++ components/index/index.vue
@@ -1,13 +1,13 @@
 <template>
     <div id="index">
         <img src="./img/logo.png" alt="" id="logo">
-        <ul class="slider">
-            <li v-for="img in newlist">
+        <ul class="slider swiper-wrapper">
+            <li v-for="img in newlist" class="swiper-slide">
                 <img v-bind:src="'static/img/index/'+img.img" title="{{img.title}}">
             </li>
         </ul>
         <ul class="slider-bar">
-            <li v-for="list in newlist">
+            <li v-for="list in newlist" @mouseenter="slideTo($index)" @mouseleave="swiperAutoPlay">
                 <span>{{list.category}}</span>
                 <time>{{list.date}}</time>
                 <h2>{{list.title}}</h2>
@@ -18,16 +18,38 @@
 
 <script type="text/babel">
     export default{
+        ready(){
+            this.swiper=new Swiper('#index', {
+                autoplay: 5000,//可选选项，自动滑动
+                effect: 'fade',
+                speed: 1000,
+                loop:true,
+                fade: {
+                    crossFade: true
+                },
+                lazyLoading: true
+            })
+        },
         data(){
             return {
                 location: 'index',
-                newlist:[
-                    {category:'正在做',date:'2015/07/23',title:'索斯光影工作室最新电影南方拍摄',img:'1.jpg'},
-                    {category:'正在做',date:'2015/07/23',title:'索斯光影工作室最新电影南方拍摄',img:'2.jpg'},
-                    {category:'正在做',date:'2015/07/23',title:'索斯光影工作室最新电影南方拍摄',img:'3.jpg'},
-                    {category:'正在做',date:'2015/07/23',title:'索斯光影工作室最新电影南方拍摄',img:'4.jpg'},
-                    {category:'正在做',date:'2015/07/23',title:'索斯光影工作室最新电影南方拍摄',img:'5.jpg'}
+                show: false,
+                newlist: [
+                    {category: '正在做', date: '2015/07/23', title: '索斯光影工作室最新电影南方拍摄', img: '1.jpg'},
+                    {category: '正在做', date: '2015/07/23', title: '索斯光影工作室最新电影南方拍摄', img: '2.jpg'},
+                    {category: '正在做', date: '2015/07/23', title: '索斯光影工作室最新电影南方拍摄', img: '3.jpg'},
+                    {category: '正在做', date: '2015/07/23', title: '索斯光影工作室最新电影南方拍摄', img: '4.jpg'},
+                    {category: '正在做', date: '2015/07/23', title: '索斯光影工作室最新电影南方拍摄', img: '5.jpg'}
                 ]
+            }
+        },
+        methods: {
+            slideTo(index){
+                this.swiper.stopAutoplay();
+                this.swiper.slideTo(index);
+            },
+            swiperAutoPlay(){
+                this.swiper.startAutoplay();
             }
         }
     }
--- components/nav/map.vue
+++ components/nav/map.vue
@@ -13,15 +13,6 @@
                 center: [116.39, 39.9]
             });
             map.setMapStyle('fresh');
-            new AMap.Marker({
-                map: map,
-                position: [116.405467, 39.907761],
-                icon: new AMap.Icon({
-                    size: new AMap.Size(40, 50),  //图标大小
-                    image: "http://webapi.amap.com/theme/v1.3/images/newpc/way_btn2.png",
-                    imageOffset: new AMap.Pixel(0, -60)
-                })
-            });
         },
         props:{
             show:{
